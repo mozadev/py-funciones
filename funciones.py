76c412f97dba21e5fa4f96498dd35afd4fdbfa92
Cesar <30559667+CesarAroon@users.noreply.github.com>
2021-02-02 16:08:15 -0500

    Add files via upload

---
+++ funciones.py
@@ -0,0 +1,33 @@
+# def imprimir_mensaje():
+#     print("mensaje especial: ")
+#     print("estoy aprendiendo a usar funciones!: ")
+
+# imprimir_mensaje()
+# imprimir_mensaje()
+# imprimir_mensaje()
+
+# def conversacion(mensaje):
+#     print('hola')
+#     print('como estas')
+#     print(mensaje)
+#     print('adios')
+
+
+
+# opcion=int(input('elige una opcion(1,2,3):'))
+# if opcion==1:
+#     conversacion('elegiste la opcion 1')
+# elif opcion==2:
+#     conversacion('elegiste la opcion 2')
+# elif opcion==3:
+#     conversacion('elegiste la opcion 3')
+# else:
+#     print('escribiste la opcion correcta')
+
+def suma(a,b):
+    print('se suman dos numeros')
+    resultado = a+b
+    return resultado
+
+sumatoria=suma(1,3)
+print(sumatoria)
